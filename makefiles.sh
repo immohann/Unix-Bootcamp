6167dae3b5ff80795e1f84f216902a77795d9600
immohann <mohanqwerty5@gmail.com>
2022-01-24 09:21:13 -0600

    comments

--- makefiles.sh
+++ makefiles.sh
@@ -1,8 +1,9 @@
+# read argument variables
 for name in $*
 do
-    
+    # create files 
     > 1000_$name.txt;
-
+    # start loop from 1 
     n=1
     # continue until $n equals 1000
     while [ $n -le 1000 ]
