44be11f99a2cfbe13662902c1e89b9bdbaf0108f
immohann <mohanqwerty5@gmail.com>
2022-02-23 00:31:27 -0600

    problem update

--- Assignment 4/conditions.sh
+++ Assignment 4/conditions.sh
@@ -1,1 +1,7 @@
-awk '{ print $9 }' FS='\ ' $1
+# awk '/MAXIMUM/' $1
+# awk '/MINIMUM/' $1
+awk '{for (I=1;I<NF;I++) if ($I == "MAXIMUM") print $(I+1)}' $1
+awk '{for (I=1;I<NF;I++) if ($I == "AVERAGE") print $(I+1)}' $1
+# awk '{for (I=1;I<NF;I++) if ($I == "MAXIMUM") ;
+#  for (J=1;J<NF;J++) if ($J == "AVERAGE") ; print($I); if ($I < $J) print("WARMER"); if ($J<$I) print("COLD")}' $1  
+
